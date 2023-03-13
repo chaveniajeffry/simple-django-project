37b5ac51946c03d10da07542b06cfb4ccb0b8f13
Jeffry Chavenia <41783921+chaveniajeffry@users.noreply.github.com>
2023-03-14 00:00:00 +0800

    feat: add accounts model

--- simpleCrud/base/models.py
+++ simpleCrud/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Accounts(models.Model):
+    account_email = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'accounts'
