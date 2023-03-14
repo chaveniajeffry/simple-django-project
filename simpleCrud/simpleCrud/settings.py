687b414eb732f804bb9d2b0758f0951c1ac003d6
Jeffry Chavenia <chaveniajeffry19@gmail.com>
2023-03-14 16:06:20 +0800

    add: more fields and change db to mysql

--- simpleCrud/simpleCrud/settings.py
+++ simpleCrud/simpleCrud/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -69,14 +70,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'simpleCrud.wsgi.application'
 
-
+AUTH_USER_MODEL = 'base.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'simplecrud',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
